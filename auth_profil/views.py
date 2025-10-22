1fe8cc5a58598babd977dd25a20a23993b41cde9
DerNiks <derrick@ui.ac.id>
2025-10-23 01:12:05 +0700

    fix: Memperbaiki bug ajax edit profile dan change password

--- auth_profil/views.py
+++ auth_profil/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth import authenticate, login, logout, update_session_auth_hash
 from django.contrib import messages
+from django.views.decorators.http import require_POST
 from django.contrib.auth.decorators import login_required
 from .forms import CustomUserCreationForm, EditProfileForm, CustomPasswordChangeForm
 from django.http import JsonResponse
@@ -55,17 +56,13 @@ def profile_view(request):
     return render(request, 'profile.html', context)
 
 @login_required
+@require_POST
 def edit_profile_view(request):
-    if request.method == 'POST' and request.headers.get('x-requested-with') == 'XMLHttpRequest':
-        form = EditProfileForm(request.POST, instance=request.user)
-        if form.is_valid():
-            form.save()
-            return JsonResponse({'status': 'success', 'message': 'Profil berhasil diperbarui!'})
-        else:
-            return JsonResponse({'status': 'error', 'errors': form.errors})
-    
-    form = EditProfileForm(instance=request.user)
-    return render(request, 'edit_profile.html', {'form': form})
+    form = EditProfileForm(request.POST, instance=request.user)
+    if form.is_valid():
+        form.save()
+        return JsonResponse({"status": "success", "message": "Profil berhasil diperbarui!"})
+    return JsonResponse({"status": "error", "errors": form.errors}, status=400)
 
 @login_required
 def delete_account_view(request):
@@ -78,13 +75,11 @@ def delete_account_view(request):
     return render(request, 'delete_account_confirm.html')
 
 @login_required
+@require_POST
 def password_change_view(request):
-    if request.method == 'POST' and request.headers.get('x-requested-with') == 'XMLHttpRequest':
-        form = CustomPasswordChangeForm(user=request.user, data=request.POST)
-        if form.is_valid():
-            user = form.save()
-            update_session_auth_hash(request, user)  
-            return JsonResponse({'status': 'success', 'message': 'Password updated successfully!'})
-        else:
-            return JsonResponse({'status': 'error', 'errors': form.errors})
-    return redirect('auth_profil:profile')
+    form = CustomPasswordChangeForm(user=request.user, data=request.POST)
+    if form.is_valid():
+        user = form.save()
+        update_session_auth_hash(request, user)
+        return JsonResponse({"status": "success", "message": "Password updated successfully!"})
+    return JsonResponse({"status": "error", "errors": form.errors}, status=400)
