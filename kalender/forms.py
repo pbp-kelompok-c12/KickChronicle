5c090ca480e703c2cd4a9eed00f82aa491b282c0
Nad-0 <go.nadine@cs.ui.ac.id>
2025-10-23 02:48:07 +0700

    feat: Mengerjakan modul kalender

--- kalender/forms.py
+++ kalender/forms.py
@@ -4,10 +4,26 @@ from .models import Kalender
 class ScheduleForm(forms.ModelForm):
     class Meta:
         model = Kalender
-        fields = ['team_1', 'team_2', 'date', 'time']
+        fields = ['team_1', 'team_1_logo', 'team_2', 'team_2_logo', 'date', 'time']
         widgets = {
-            'date': forms.DateInput(attrs={'type': 'date', 'class': 'w-full rounded-md border-gray-300'}),
-            'time': forms.TimeInput(attrs={'type': 'time', 'class': 'w-full rounded-md border-gray-300'}),
-            'team_1': forms.TextInput(attrs={'class': 'w-full rounded-md border-gray-300'}),
-            'team_2': forms.TextInput(attrs={'class': 'w-full rounded-md border-gray-300'}),
+            'team_1': forms.TextInput(attrs={
+                'class': 'w-full rounded-md border-gray-800'
+            }),
+            'team_1_logo': forms.URLInput(attrs={
+                'class': 'w-full rounded-md border-gray-800'
+            }),
+            'team_2': forms.TextInput(attrs={
+                'class': 'w-full rounded-md border-gray-800'
+            }),
+            'team_2_logo': forms.URLInput(attrs={
+                'class': 'w-full rounded-md border-gray-800'
+            }),
+            'date': forms.DateInput(attrs={
+                'type': 'date',
+                'class': 'w-full rounded-md border-gray-800'
+            }),
+            'time': forms.TimeInput(attrs={
+                'type': 'time',
+                'class': 'w-full rounded-md border-gray-800'
+            }),
         }
--- kalender/urls.py
+++ kalender/urls.py
@@ -4,8 +4,10 @@ from . import views
 app_name = 'kalender'
 
 urlpatterns = [
-    path("", views.show_calendar_page, name="schedule_index"),
-    path("api/matches/", views.get_matches_api, name="get_matches_api"),
-    path("export/<int:kalender_id>/", views.export_kalender_ics, name="export_kalender_ics"),
-    path("tambah/", views.add_schedule_view, name="add_schedule"),
+    path('', views.show_calendar_page, name='schedule_index'),
+    path('add/', views.add_schedule_view, name='add_schedule'),
+    path('edit/<int:pk>/', views.edit_schedule_view, name='edit_schedule'),
+    path('delete/<int:pk>/', views.delete_schedule_view, name='delete_schedule'),
+    path('api/get_matches/', views.get_matches_api, name='get_matches_api'),
+    path('export/<int:kalender_id>/', views.export_kalender_ics, name='export_kalender_ics'),
 ]
